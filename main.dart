dd947f5af6e9c95504238911e6f70ec31195ff2d
gtins <111321003+gtins@users.noreply.github.com>
2024-04-08 10:28:37 -0300

    Update main.dart
    list with players hands (only one card so far)

--- main.dart
+++ main.dart
@@ -18,36 +18,53 @@ class Player {
 }
 
 class Versus {
+  //List<Card> hands;
   List<Card> handOne;
   List<Card> handTwo;
 
   Versus(this.handOne, this.handTwo);
-
+  //Versus(this.hands);
   String compareHands() {
-    int cardOne = handOne[0].number;
-    int cardTwo = handTwo[0].number;
+    int previous = handOne[0].number;
+    int current = handTwo[0].number;
 
-    if (cardOne > cardTwo) {
+    if (previous > current) {
       return 'Player One wins!';
-    } else if (cardOne < cardTwo) {
+    } else if (previous < current) {
       return 'Player Two wins!';
     } else {
       return 'It\'s a tie!';
     }
   }
+  // String compareHands() {
+
+  // }
 }
 
 void main() {
-    Player playerOne = Player('Gustavo', Card(3,'Spades'));
-    for (var card in playerOne.hand) {
-    print('${card.number} of ${card.suit}');//should turn this into a class to make a dev's life easier
-  }
+    List<List<Card>> hands = [];
+    Player playerOne = Player('Gustavo', Card(4,'Clubs'));
+    hands.add(playerOne.hand);
   
-    Player playerTwo = Player('Maruan', Card(2, 'Clubs'));
-    for (var card in playerTwo.hand) {
-    print('${card.number} of ${card.suit}');
+    Player playerTwo = Player('Maruan', Card(3, 'Spades'));
+    hands.add(playerTwo.hand);
+
+    Player playerThree = Player('Icaro', Card(2,'Hearts'));
+    hands.add(playerThree.hand);
+
+    Player playerFour = Player('Victor', Card(1,'Diamonds'));
+    hands.add(playerFour.hand);
+  
+  for (var hand in hands){
+      for (var card in hand){
+        print('${card.number} of ${card.suit}');
+      }
     }
   
   Versus versus = Versus(playerOne.hand, playerTwo.hand);
-  print(versus.compareHands()); // still need to figure out a way of making this repeat for each card in the hand
+  //print(versus.compareHands());
+  // Versus versus = Versus(player)
 }
+
+
+
