7a8e5ad223b1a372a83c363722ea5e650a75d395
gtins <111321003+gtins@users.noreply.github.com>
2024-04-16 15:02:33 -0300

    Update versus.dart
    adding the comparison of suits for the cases where both playes have the same number

--- versus.dart
+++ versus.dart
@@ -11,13 +11,33 @@ class Versus {
       int currentWinner = 0;
       for (int j = 1; j < players.length; j++) {
         if (players[j].hand[i].cardRank > players[currentWinner].hand[i].cardRank) {
-          
-          currentWinner = j;
+         currentWinner = j;
+        } else if(players[j].hand[i].cardRank == players[currentWinner].hand[i].cardRank){
+          String suitWinner = compareSuits([players[j].hand[i], players[currentWinner].hand[i]]);
+            if(suitWinner == players[j].hand[i].suit){
+              currentWinner = j;
+            } else if(suitWinner == "Tie"){
+              comparedResults.add("Tie!");
+              return comparedResults;
+            }
         }
       }
       comparedResults.add(
           'Player ${players[currentWinner].name} wins with card ${players[currentWinner].hand[i].number}');
     }
     return comparedResults;
+  }
+
+  String compareSuits(List<Card> tiedCards) {
+    String winningSuit = tiedCards[0].suit;
+    for (int i = 1; i < tiedCards.length; i++) {
+      if (tiedCards[i].suitRank > tiedCards[0].suitRank) {
+        winningSuit = tiedCards[i].suit;
+        return winningSuit;
+      } else if (tiedCards[i].suitRank == tiedCards[0].suitRank){
+        return "Tie";
+      }
+    }
+    return winningSuit;
   }
 }
