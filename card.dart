bb3f9bcb8720acf26bc971496b5756c806d60eca
gtins <111321003+gtins@users.noreply.github.com>
2024-04-16 13:55:08 -0300

    Update card.dart
    rank of cards, truco doesn't follow the normal order of a deck

--- card.dart
+++ card.dart
@@ -1,7 +1,38 @@
 class Card {
   final int number;
   final String suit;
+  int get cardRank {
+    switch (number) {
+      case 1:
+        return 8; // as
+      case 2: 
+        return 9; // 2
+      case 3:
+        return 10; // 3(mais forte)
+      case 4:
+        return 1; // 4
+      case 5: 
+        return 2; // 5
+      case 6: 
+        return 3; // 6
+      case 7:
+        return 4; // 7
+      case 10: 
+        return 5; // J
+      case 11: 
+        return 6; // Q
+      case 12: 
+        return 7; // K
+      default: 
+        return 0; 
+    }
+  }
 
+  @override
+  String toString() {
+    return '$number de $suit';
+  }
+  
   Card(this.number, this.suit);
   //create card, cart must go somewhere
 }
